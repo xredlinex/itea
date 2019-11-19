0e886135a2904ebce56b8ad618b83f49bc06c98c
xredlinex <redik.line@gmail.com>
2019-11-20 00:23:10 +0200

    make develop

---
+++ IteaIOSDip/Courses/CoursesViewController+Extension.swift
@@ -0,0 +1,9 @@
+//
+//  CoursesViewController+Extension.swift
+//  IteaIOSDip
+//
+//  Created by alexey sorochan on 11/20/19.
+//  Copyright © 2019 alexey sorochan. All rights reserved.
+//
+
+import Foundation
