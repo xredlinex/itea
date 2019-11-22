fae57ac4237ff92b029acd4331e5062c8268c943
xredlinex <redik.line@gmail.com>
2019-11-22 14:24:47 +0200

    push to dev all

--- IteaIOSDip/CoursesInit/Courses.swift
+++ IteaIOSDip/CoursesInit/Courses.swift
@@ -8,30 +8,6 @@
 
 import UIKit
 
-func makeQaCourses() -> [Courses] {
-    let qaBase = Courses()
-    qaBase.courseName = "Базовый курс QA"
-    qaBase.courseDescription = qaCoursesDescrioption()[0].courseDescriotion
-    qaBase.courseSkills = qaCoursesDescrioption()[0].courseSkills
-    qaBase.courseProgram = qaCoursesDescrioption()[0].courseProgram
-    qaBase.courseImage = "qaBase"
-   
-    let qaAdvanced = Courses()
-    qaAdvanced.courseName = "Продвинутый курс QA"
-    qaAdvanced.courseDescription = qaCoursesDescrioption()[0].courseDescriotion
-    qaAdvanced.courseSkills = qaCoursesDescrioption()[0].courseSkills
-    qaAdvanced.courseProgram = qaCoursesDescrioption()[0].courseProgram
-    qaAdvanced.courseImage = "qaAdvanced"
-    
-    let qaAutomation = Courses()
-    qaAutomation.courseName = "QA Automation"
-    qaAutomation.courseDescription = qaCoursesDescrioption()[0].courseDescriotion
-    qaAutomation.courseSkills = qaCoursesDescrioption()[0].courseSkills
-    qaAutomation.courseProgram = qaCoursesDescrioption()[0].courseProgram
-    qaAutomation.courseImage = "qaAutomation"
-     
-    return [qaBase, qaAdvanced, qaAutomation]
-}
 
 
 
